bea4a8229c31fc8545c93da0c7fbb52c099a4065
weeee10101 <weeee10101@gmail.com>
2025-03-12 12:47:03 +0900

    Add files via upload

---
+++ 20250312노우철.sql
@@ -0,0 +1,10 @@
+create table address(
+address1 varchar(255),
+address2 varchar(255),
+address3 varchar(255),
+postal_code varchar(255),
+client_id varchar(255));
+
+insert into address values('안양시','동안구','학의로','12345','노우철'); //테이블 삽입
+
+drop table address; //테이블 삭제
